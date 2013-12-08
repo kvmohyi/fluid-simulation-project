d8abad3fd9ee85f312875115e1c2ce17dbd1d8a6
Jonathan_Ma <cs184-cy@hive2.CS.Berkeley.EDU>
2013-12-08 13:25:20 -0800

    fixed equations

--- equations.hpp
+++ equations.hpp
@@ -10,7 +10,7 @@ using namespace glm;
 float distance(Particle particle, Particle other);
 float gaussian_smoothing(Particle particle, Particle other);
 vec3 gradient_gaussian_smoothing(Particle particle, Particle other);
-vec3 gradient2_gaussian_smoothing(Particle particle, Particle other);
+float gradient2_gaussian_smoothing(Particle particle, Particle other);
 vec3 force_pressure(Particle particle, Particle other);
 vec3 force_viscosity(Particle particle, Particle other);
 
